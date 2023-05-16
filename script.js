3a5b2e2d889cc9122dbed45238c9ce7d0496f2f3
Brian <111296104+Brianvictor@users.noreply.github.com>
2023-05-16 16:50:14 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+function sim() {
+    alert("❤️ TAMBÉM ESTOU COM SAUDADES ❤️ :)");
+    // redireciona para um URL apos aceitar
+    location.href="https://www.youtube.com/watch?v=VztMsyyRiuw";
+}
+
+function desvia(t) {
+    var btn = t;
+    btn.style.position = 'absolute';
+    btn.style.bottom = geraPosicao(10, 90);
+    btn.style.left = geraPosicao(10, 90);
+    console.log("opa, desviei...");
+}
+
+function geraPosicao(min, max) {
+    return (Math.random() * (max - min) + min) + "%";
+}
